cd8230df7db15ae8f63bdbde089cc64a45f511d5
Harshit <harshit.brizwasi@gmail.com>
2025-11-11 19:58:50 +0530

    setup todo controller

---
+++ src/main/java/com/example/demo/ToDoController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ToDoController {
+    private static List<ToDo> todos;
+    public ToDoController() {
+        todos = new ArrayList<>();
+        todos.add(new ToDo(1,false,"Todo 1", 1));
+    }
+    
+}
