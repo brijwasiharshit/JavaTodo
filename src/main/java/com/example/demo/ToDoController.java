87956faee6b98cec86a5b0ef4a4594bd8e4fa6b3
Harshit <harshit.brizwasi@gmail.com>
2025-11-11 21:11:34 +0530

    Configured Reponse Entity

--- src/main/java/com/example/demo/ToDoController.java
+++ src/main/java/com/example/demo/ToDoController.java
@@ -1,15 +1,28 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class ToDoController {
-    private static List<ToDo> todos;
+    private static List<ToDo> todoList;
     public ToDoController() {
-        todos = new ArrayList<>();
-        todos.add(new ToDo(1,false,"Todo 1", 1));
+        todoList = new ArrayList<>();
+        todoList.add(new ToDo(1,false,"Todo 1", 1));
+        todoList.add(new ToDo(2,false,"Todo 2", 1));
     }
-    
+    @GetMapping("/todos")
+    public ResponseEntity<List<ToDo>>  getTodos(){
+        return ResponseEntity.ok(todoList);
+    }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/todos")
+    public ResponseEntity<ToDo> createTodo(@RequestBody ToDo newToDo){
+    todoList.add(newToDo);
+    return ResponseEntity.status(HttpStatus.CREATED).body(newToDo);
+    }
 }
